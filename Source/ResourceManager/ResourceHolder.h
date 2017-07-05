4f639c5ffd6f921fffbea655727d2e2c1addd47f
Hopson97 <mhopson@hotmail.co.uk>
2017-07-05 23:45:05 +0100

    Improve singletonness

--- Source/ResourceManager/ResourceHolder.h
+++ Source/ResourceManager/ResourceHolder.h
@@ -6,24 +6,25 @@
 
 #include "ResourceManager.h"
 
-struct ResourceHolder
+class ResourceHolder
 {
-    static ResourceHolder& get()
-    {
-        static ResourceHolder res;
-        return res;
-    }
+    public:
+        static ResourceHolder& get()
+        {
+            static ResourceHolder res;
+            return res;
+        }
 
-    ResourceHolder() noexcept
-    :   textures        ("res/textures/",   "png")
-    ,   fonts           ("res/fonts/",      "ttf")
-    ,   soundBuffers    ("res/sfx",         "ogg")
-    { }
+        ResourceManager<sf::Texture>        textures;
+        ResourceManager<sf::Font>           fonts;
+        ResourceManager<sf::SoundBuffer>    soundBuffers;
 
-    ResourceManager<sf::Texture>        textures;
-    ResourceManager<sf::Font>           fonts;
-    ResourceManager<sf::SoundBuffer>    soundBuffers;
-
+    private:
+        ResourceHolder() noexcept
+        :   textures        ("res/textures/",   "png")
+        ,   fonts           ("res/fonts/",      "ttf")
+        ,   soundBuffers    ("res/sfx",         "ogg")
+        { }
 };
 
 #endif // RESOURCEHOLDER_INCLUDED
